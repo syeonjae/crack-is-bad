1b03db596a8cbc0287b84819fefcde3231c09e73
DESKTOP-0UVNTA3\syeonjae <syeonjae98@gmail.com>
2023-12-10 23:48:17 +0900

    Feat : 디자인 추가

--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,1 +1,9 @@
-
+document.addEventListener("DOMContentLoaded", () => {
+  const fileBox = document.querySelector("#file");
+  const fileName = document.querySelector(".upload-name");
+  if (!fileBox || !fileName) return;
+  fileBox.addEventListener("change", () => {
+    // @ts-ignore
+    fileName.value = fileBox.value;
+  });
+});
